bea072e73ff5c97e941b016ae58b4cba53693c37
Andreas M Simanullang <91534680+Andreas-23@users.noreply.github.com>
2024-03-07 11:43:08 +0700

    Update simple_llm.py

--- simple_llm.py
+++ simple_llm.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 import gradio as gr
 
 # Mengatur environment variable "OPENAI_API_KEY" dengan OpenAI API key milikmu. ini diperlukan untuk proses autentikasi ke OpenAI API.
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY"
 
 # Mendefinisikan jenis model 
 gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )

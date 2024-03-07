9f3d0dbaaf7c3505174dc8a01d3aaf8c64576d3d
Andreas M Simanullang <91534680+Andreas-23@users.noreply.github.com>
2024-03-07 11:43:21 +0700

    Update prompt_with_template.py

--- prompt_with_template.py
+++ prompt_with_template.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 import os
 from langchain_openai import ChatOpenAI
 
-openai_api_key = "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"
+openai_api_key = "YOUR_API_KEY"
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # Mendefinisikan model AI
